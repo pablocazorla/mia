7db8820a2936743771676a5107d0c993e804b2f3
Pablo Cazorla <pablo.david.cazorla@gmail.com>
2015-01-27 09:52:36 -0300

    reDes

--- archive-design.php
+++ archive-design.php
@@ -27,13 +27,18 @@ if (!$async){
 				</div>	
 			</div>
 		</header>
-		<section id="gallery" class="clearfix wrap gallery-design">
+		<section id="gallery" class="clearfix gallery-design">
 		<?php
 			$alt = -1;
+			$backCol = 1;
 			$list = new WP_Query('post_type=design&posts_per_page=64');
 			if ($list->have_posts()): while ($list->have_posts()): $list->the_post(); ?>			
 			<?php
 				$alt *= -1;
+				$backCol += 1;
+				if($backCol > 8){
+					$backCol = 1;
+				}
 				$types = get_the_terms( $post->ID, 'design' );
 				$classType = '';										
 				if ( $types && ! is_wp_error( $types ) ) {
@@ -42,28 +47,29 @@ if (!$async){
 					}
 				}
 			?>
-			<div class="gallery-fig clearfix design-pod <?php echo $classType; if($alt < 0){ echo ' alt';}?> soft-light slg-bottom">
-				<div class="design-box design-image">
-					<?php if($classType == ' webdesign'){ ?>
-					<img class="design-pc" src="<?php bloginfo('template_url'); ?>/img/design-pc.png"/>
-					<?php }?>
-					<figure class="<?php if($classType == ' webdesign'){echo 'wd-figure';}?>">
-				        <?php if(has_post_thumbnail()){
-						echo '<img class="design-thumb-img srcwait" src="" srcwait="' . url_thumbnail('design-thumb') .'">';
-						} ?>
-						<a href="<?php the_permalink(); ?>" rel="<?php the_ID();?>" >
-							<figcaption><span>More</span></figcaption>
-						</a>							
-					</figure>										
-				</div>
-				<div class="design-box design-gap"></div>
-				<div class="design-box design-text">
-					<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-					<p class="design-category"><?php echo $classType;?></p>
-					<?php the_excerpt();?>
+			<div class="gallery-fig <?php echo ' backcolor'.$backCol; echo $classType; if($alt < 0){ echo ' alt';}?>">
+				<div class="wrap clearfix design-pod soft-light slg-bottom">
+					<div class="design-box design-image">
+						<?php if($classType == ' webdesign'){ ?>
+						<img class="design-pc" src="<?php bloginfo('template_url'); ?>/img/design-pc.png"/>
+						<?php }?>
+						<figure class="<?php if($classType == ' webdesign'){echo 'wd-figure';}?>">
+					        <?php if(has_post_thumbnail()){
+							echo '<img class="design-thumb-img srcwait" src="" srcwait="' . url_thumbnail('design-thumb') .'">';
+							} ?>
+							<a href="<?php the_permalink(); ?>" rel="<?php the_ID();?>" >
+								<figcaption><span>More</span></figcaption>
+							</a>							
+						</figure>										
+					</div>
+					<div class="design-box design-gap"></div>
+					<div class="design-box design-text">
+						<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+						<p class="design-category"><?php echo $classType;?></p>
+						<?php the_excerpt();?>
+					</div>
 				</div>
 			</div>
-
 			<?php endwhile; ?>
 			<?php wp_reset_postdata(); ?>
 			<?php else :?>
